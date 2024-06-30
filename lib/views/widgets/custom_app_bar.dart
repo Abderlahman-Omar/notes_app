09086c4bd336539d7510483108efa2287b0f65b0
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2024-07-01 02:38:10 +0300

    edit note done

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, this.title, this.icon});
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.icon,
+    this.onPressed,
+  });
   final String? title;
   final IconData? icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -17,6 +23,7 @@ class CustomAppBar extends StatelessWidget {
         ),
         const Spacer(),
         CustomIcon(
+          onPressed: onPressed,
           icon: icon,
         ),
       ],
--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomIcon extends StatelessWidget {
-  const CustomIcon({super.key, this.icon});
+  const CustomIcon({super.key, this.icon, this.onPressed});
   final IconData? icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,9 +18,12 @@ class CustomIcon extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Icon(
-          icon,
-          size: 28,
+        child: IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: 28,
+          ),
         ),
       ),
     );
